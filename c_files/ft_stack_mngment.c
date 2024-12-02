d83d3f9d67b13608dc30cd6c0ee0d9ae7b74d59f
Ilan Levy <ilevy@bess-f4r3s10.clusters.42paris.fr>
2024-12-02 16:23:34 +0100

    stack_management.c

--- c_files/ft_stack_mngment.c
+++ c_files/ft_stack_mngment.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_organize.c                                      :+:      :+:    :+:   */
+/*   ft_stack_mngment.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ilevy <ilevy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/02 13:15:43 by ilevy             #+#    #+#             */
-/*   Updated: 2024/12/02 16:22:44 by ilevy            ###   ########.fr       */
+/*   Created: 2024/12/02 16:23:22 by ilevy             #+#    #+#             */
+/*   Updated: 2024/12/02 16:23:26 by ilevy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/h_files/libft.h"
-
-void	ft_organize_three(t_stack *stack)
-{
-	int	i;
-	int	max;
-	int	min;
-
-	i = 0;
-	max = ft_find_max_stack(&stack);
-	min = ft_find_min_stack(&stack);
-	
-
-}
- //Possible to only use one function by returning both max and min with an int tab;
-
-
-
+i;
